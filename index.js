6736983406a270f9c975eda25f16cf8ae11b3921
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-19 04:38:02 +0600

    basics of redux: using subscribe

--- index.js
+++ index.js
@@ -14,17 +14,26 @@ const initState = {
 // }
 
 const rootReducer = (state = initState, action) => {
+
     if (action.type === 'INC_NUM') {
         return {
             // spread operator
             ...state,
-            num: state.num + 1
+            num: state.num + 2
         }
     }
+
     if (action.type === 'ADD_NUM') {
         return {
             ...state,
             num: state.num + action.a
+        }
+    }
+
+    if (action.type === 'DEC_NUM') {
+        return {
+            ...state,
+            num: state.num - 1
         }
     }
     return state;
@@ -37,16 +46,29 @@ const store = redux.createStore(rootReducer);
 
 console.log(store.getState());
 
+// Subscribe
+// after dispatch subscribe will be called
+// if dispatch is not called, subscribe will not called
+store.subscribe(() => {
+    console.log("[Subscribe]", store.getState());
+})
+
 // Dispatching Action
-store.dispatch({
-    // type is a built-in object
-    type: 'INC_NUM'
-});
+// if we change the sequence of actions, subscribe will be called according to that sequence
 
 store.dispatch({
     type: 'ADD_NUM',
     a: 34
 })
 
+store.dispatch({
+    type: 'DEC_NUM'
+})
+
+store.dispatch({
+    // type is a built-in object
+    type: 'INC_NUM'
+});
+
 // console.log(store);
-console.log(store.getState());
+// console.log(store.getState());
